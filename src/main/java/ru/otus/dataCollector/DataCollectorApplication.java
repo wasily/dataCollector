306fc9e43c5cef73c459a8e03932149df2a9e314
was1ly <was1ly@yandex.ru>
2019-10-10 18:34:38 +0300

    empty project imported

---
+++ src/main/java/ru/otus/dataCollector/DataCollectorApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.dataCollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCollectorApplication.class, args);
+	}
+
+}
