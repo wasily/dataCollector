c2f0858b9eea612ef7ed45882d11dc891bc29edc
was1ly <was1ly@yandex.ru>
2019-10-23 16:12:45 +0300

    removed unused repository

--- src/main/java/ru/otus/dataCollector/repositories/SubscribedReleaseRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.dataCollector.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import ru.otus.dataCollector.model.domain.SubscribedRelease;
-
-public interface SubscribedReleaseRepository extends MongoRepository<SubscribedRelease, String> {
-    void deleteByImdbId(String imdbId);
-}
